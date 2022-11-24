66adf9353872c4b209928466991bc3dc466c0a61
Zachary Mathis <zachamathis@gmail.com>
2022-11-23 17:16:30 -0800

    created components and added prerequisites

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,5 @@
+export default function NavBar(props) {
+    return (
+        <nav></nav>
+    )
+}
---
+++ src/Components/Posts/Post.js
@@ -0,0 +1,7 @@
+export default function Post(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}

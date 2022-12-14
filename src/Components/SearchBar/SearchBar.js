2850df5760692210f1d73033d752c990bae69107
Zachary Mathis <zachamathis@gmail.com>
2022-12-14 14:18:55 -0800

    SearchBar submit now working as intended

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,16 +2,28 @@ import { useState } from "react"
 
 export default function SearchBar(props) {
     const [ term, setTerm ] = useState('')
+    const { setSearchTerm } = props
     
     const handleChange = (e) => {
         setTerm(e.target.value)
-        props.setSearchTerm(`r/all/search.json?q=${term}&restrict_sr=on&include_over_18=on&sort=relevance&t=all`)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+        setSearchTerm(`r/all/search.json?q=${term}&restrict_sr=on&include_over_18=off&sort=relevance&t=all`)
+        
+        if(term === '') {
+            setSearchTerm('r/popular.json')
+        }
     }
     
     return (
         <div>
             <h1>test</h1>
-            <input value={term} type="text" placeholder="Search here" onChange={handleChange}></input><button>🔎</button>
+            <form onSubmit={handleSubmit}>
+                <input value={term} type="text" placeholder="Search here" onChange={handleChange}></input><button>🔎</button>
+            </form>
         </div>
     )
 }

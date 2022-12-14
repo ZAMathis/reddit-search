582e489a12dade02ccf90102a217b5b6a418ef5a
Zachary Mathis <zachamathis@gmail.com>
2022-12-13 16:26:47 -0800

    search glass emoji :p

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@ export default function SearchBar(props) {
     return (
         <div>
             <h1>test</h1>
-            <input placeholder="Search here"></input><button></button>
+            <input placeholder="Search here"></input><button>🔎</button>
         </div>
     )
 }
